ae7bc31d7898eeddc2d6a0ff55f894d4095d2bd3
trickell <wku.madrigal@gmail.com>
2026-01-06 11:18:11 -0600

    Port and host added to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 8080, // Set your desired port here
+    strictPort: true,
   },
 })
